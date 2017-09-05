3f52cc62573c49ae2d7569cae03af11b236689b7
Ignas Grigaravicius <kickeris@gmail.com>
2017-09-05 22:43:48 +0100

    Firebase auth added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule }from 'angularfire2';
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyBFc_X-FshQrpJirlDOUTXvuiNu8PzXgIg",
+    authDomain: "angulartwologin.firebaseapp.com",
+    databaseURL: "https://angulartwologin.firebaseio.com",
+    storageBucket: "angulartwologin.appspot.com",
+    messagingSenderId: "460855562735"
+};
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +19,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
